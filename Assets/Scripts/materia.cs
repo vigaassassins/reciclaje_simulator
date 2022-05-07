6fc703374acaacaaf71b285095817c46d23cfa10
vigaassassins <81651887+vigaassassins@users.noreply.github.com>
2022-05-07 18:25:39 -0500

    material se puede soltar

--- Assets/Scripts/materia.cs
+++ Assets/Scripts/materia.cs
@@ -7,9 +7,11 @@ public class materia : MonoBehaviour
     machine gc;
     // Start is called before the first frame update
     bool inmachine;
+    private Rigidbody2D rb2d;
     void Start()
     {
         inmachine = true;
+        rb2d = GetComponent<Rigidbody2D>();
         GameObject obj = GameObject.FindGameObjectWithTag("machine");
         gc = obj.GetComponent<machine>();
     }
@@ -17,7 +19,13 @@ public class materia : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (inmachine)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inmachine = false;
+            rb2d.gravityScale=1;
+        }
+            //rb2d.gravityScale (addGrav);
+            if (inmachine)
         {
             transform.position = gc.transform.position;
         }
